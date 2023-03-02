9c4149d631e03b02b92f669e4288901c03234d45
novraedi <novraepe@gmail.com>
2023-03-02 14:50:01 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import WelcomeItem from "./components/WelcomeItem.vue";
+import ProductDisplay from "./components/ProductDisplay.vue";
 </script>
 
 <template>
-  <WelcomeItem />
+  <!-- <WelcomeItem /> -->
+  <ProductDisplay />
 </template>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-import './assets/main.css'
-
-createApp(App).mount('#app')
+createApp(App).mount("#app");
